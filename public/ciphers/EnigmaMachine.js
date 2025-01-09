378a5afeed1e4f120d201000445981e7710e199a
Martyn Bissett <martyn.bissett@strath.ac.uk>
2025-01-09 19:13:57 +0000

    Add enigma machine cipher

--- public/ciphers/EnigmaMachine.js
+++ public/ciphers/EnigmaMachine.js
@@ -1,4 +1,4 @@
-export default class MultiplicativeCipher {
+export default class EnigmaMachine {
 
   description = `<p>The Enigma machine was a sophisticated encryption device used primarily by Nazi Germany during World War II to secure military communications. It employed a system of rotors and plugboards to generate complex polyalphabetic ciphers, making it one of the most advanced mechanical cipher systems of its time. Despite its perceived invulnerability, the machine's codes were famously broken by Allied cryptographers, significantly influencing the outcome of the war.</p>
   <p>TODO</p>`;
@@ -12,55 +12,138 @@ export default class MultiplicativeCipher {
     <label for="plugboard" class="form-label">Plugboard</label>
     <input class="form-control" value="ab cd ef" id="plugboard" />`;
 
-  // // TODO use this with regular expression valid chars
-  // #alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
   constructor(parameters) {
-    this.parameters = parameters
+    this.parameters = parameters;
+    this.rotors = [
+      "EKMFLGDQVZNTOWYHXUSPAIBRCJ", // Rotor I
+      "AJDKSIRUXBLHWTMCQGZNPYFVOE", // Rotor II
+      "BDFHJLCPRTXVZNYEIWGAKMUSQO"  // Rotor III
+    ];
+    this.reflector = "YRUHQSLDPXNGOKMIEBFZCWVJAT"; // Reflector B
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
-  // /**
-  //  * Return substitution character
-  //  * @param {String} char 
-  //  * @returns {String}
-  //  */
-  // #encipherChar(char, key) {
-  //   const charNum = this.#alphabet.indexOf(char)
-  //   const shiftedNum = charNum * key;
-  //   const shiftedChar = this.#alphabet[modular(shiftedNum, this.#alphabet.length)];
-  //   return shiftedChar;
-  // }
-
-  // /**
-  //  * Return substitution character
-  //  * @param {String} char 
-  //  * @returns {String}
-  //  */
-  // #decipherChar(char, key) {
-  //   const mod = this.#alphabet.length
-  //   const charNum = this.#alphabet.indexOf(char);    
-  //   const shiftedNum = charNum * modularInverse(key, this.#alphabet.length);
-  //   const shiftedChar = this.#alphabet[modular(shiftedNum, this.#alphabet.length)];
-  //   return shiftedChar;
-  // }
+  /**
+   * Substitute a character using the plugboard mapping.
+   * @param {String} char - The character to substitute.
+   * @param {String} plugboard - Plugboard mappings in "AB CD EF" format.
+   * @returns {String} - The substituted character.
+   */
+  #applyPlugboard(char, plugboard) {
+    const swaps = plugboard.split(" ").map(pair => pair.split(""));
+    for (const [a, b] of swaps) {
+      if (char === a) return b;
+      if (char === b) return a;
+    }
+    return char;
+  }
 
   /**
-   * Return an string of ciphertext from plaintext
-   * @param {String} plaintext 
-   * @returns {String}
+   * Perform a forward substitution through the rotors.
+   * @param {String} char - The character to encipher.
+   * @param {Array} rotorPositions - The positions of the rotors.
+   * @returns {String} - The substituted character.
+   */
+  #encipherChar(char, rotorPositions) {
+    const { rotors, reflector, alphabet } = this;
+
+    // Pass through plugboard
+    const plugboard = this.parameters.querySelector("#plugboard").value;
+    char = this.#applyPlugboard(char, plugboard);
+
+    // Forward through rotors
+    for (let i = 0; i < rotors.length; i++) {
+      const position = rotorPositions[i];
+      const index = (alphabet.indexOf(char) + position) % 26;
+      char = rotors[i][index];
+    }
+
+    // Reflector
+    char = reflector[alphabet.indexOf(char)];
+
+    // Backward through rotors (reverse order)
+    for (let i = rotors.length - 1; i >= 0; i--) {
+      const position = rotorPositions[i];
+      const index = (rotors[i].indexOf(char) - position + 26) % 26;
+      char = alphabet[index];
+    }
+
+    // Pass through plugboard again
+    char = this.#applyPlugboard(char, plugboard);
+
+    return char;
+  }
+
+  /**
+   * Perform a backward substitution through the rotors.
+   * In Enigma, enciphering and deciphering use the same process.
+   * @param {String} char - The character to decipher.
+   * @param {Array} rotorPositions - The positions of the rotors.
+   * @returns {String} - The deciphered character.
+   */
+  #decipherChar(char, rotorPositions) {
+    // Deciphering in Enigma is symmetric to enciphering.
+    return this.#encipherChar(char, rotorPositions);
+  }
+
+  /**
+   * Advance the rotor positions, simulating Enigma's stepping mechanism.
+   * @param {Array} rotorPositions - The positions of the rotors.
+   */
+  #advanceRotors(rotorPositions) {
+    rotorPositions[0] = (rotorPositions[0] + 1) % 26;
+    if (rotorPositions[0] === 0) {
+      rotorPositions[1] = (rotorPositions[1] + 1) % 26;
+      if (rotorPositions[1] === 0) {
+        rotorPositions[2] = (rotorPositions[2] + 1) % 26;
+      }
+    }
+  }
+
+  /**
+   * Encipher a string of plaintext.
+   * @param {String} plaintext - The plaintext to encipher.
+   * @returns {String} - The resulting ciphertext.
    */
   encipher(plaintext) {
-    // const key = this.parameters.querySelector("#key").value
-    // return plaintext.replace(/[^A-Za-z]/gi, '').split("").map((char) => this.#encipherChar(char.toUpperCase(), parseInt(key))).join("");
+    const rotor1 = parseInt(this.parameters.querySelector("#rotor1").value, 10);
+    const rotor2 = parseInt(this.parameters.querySelector("#rotor2").value, 10);
+    const rotor3 = parseInt(this.parameters.querySelector("#rotor3").value, 10);
+    const rotorPositions = [rotor1, rotor2, rotor3];
+
+    return plaintext
+      .replace(/[^A-Za-z]/g, "")
+      .toUpperCase()
+      .split("")
+      .map(char => {
+        const encipheredChar = this.#encipherChar(char, rotorPositions);
+        this.#advanceRotors(rotorPositions);
+        return encipheredChar;
+      })
+      .join("");
   }
 
   /**
-   * Return a string of plaintext from ciphertext
-   * @param {String} ciphertext 
-   * @returns {String}
+   * Decipher a string of ciphertext.
+   * @param {String} ciphertext - The ciphertext to decipher.
+   * @returns {String} - The resulting plaintext.
    */
   decipher(ciphertext) {
-    // const key = this.parameters.querySelector("#key").value    
-    // return ciphertext.replace(/[^A-Za-z]/gi, '').split("").map((char) => this.#decipherChar(char.toUpperCase(), parseInt(key))).join("");
+    const rotor1 = parseInt(this.parameters.querySelector("#rotor1").value, 10);
+    const rotor2 = parseInt(this.parameters.querySelector("#rotor2").value, 10);
+    const rotor3 = parseInt(this.parameters.querySelector("#rotor3").value, 10);
+    const rotorPositions = [rotor1, rotor2, rotor3];
+
+    return ciphertext
+      .replace(/[^A-Za-z]/g, "")
+      .toUpperCase()
+      .split("")
+      .map(char => {
+        const decipheredChar = this.#decipherChar(char, rotorPositions);
+        this.#advanceRotors(rotorPositions);
+        return decipheredChar;
+      })
+      .join("");
   }
+
 }
